f111f14987cc34a247d64db465955dba9e795997
Basu0807 <vasupandeyji.98@gmail.com>
2024-04-12 18:26:47 +0530

    data updated

--- frontend/src/pages/Job_Seeker/MyJobs/Applied.js
+++ frontend/src/pages/Job_Seeker/MyJobs/Applied.js
@@ -5,34 +5,93 @@ import {useNavigate}from 'react-router-dom'
 const Applied = () => {
     const navi=useNavigate()
     const [applied_jobs] = useState([
-    {
-      img: "https://i.pinimg.com/736x/0a/06/60/0a06600cc3cedeb49280b54114c88ce6.jpg",
-      jobrole: "Software Engineer",
-      lpa: 30,
-      location: "Indore",
-      category: "Fresher",
-    },
-    {
-      img: "https://logos-world.net/wp-content/uploads/2020/11/Flipkart-Emblem.png",
-      jobrole: "Software Engineer",
-      lpa: 30,
-      location: "Indore",
-      category: "Fresher",
-    },
-    {
-      img: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
-      jobrole: "Software Engineer",
-      lpa: 30,
-      location: "Indore",
-      category: "Fresher",
-    },
-    {
-      img: "https://i.pinimg.com/736x/0a/06/60/0a06600cc3cedeb49280b54114c88ce6.jpg",
-      jobrole: "Software Engineer",
-      lpa: 30,
-      location: "Indore",
-      category: "Fresher",
-    },
+        {
+            "_id": "6618f1305502f224a4e5b551",
+            "jobTitle": "Android Developer",
+            "jobPoster": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQy5IY6HmILZ8AgoXUijtQsg3xO1BR__fPkGouHAaC05Q&s",
+            "jobDescription": "Develop native Android applications. Experience with Java, Kotlin, and the Android SDK. Strong understanding of mobile development",
+            "employmentType": "Contract",
+            "location": "Gurgaon, India",
+            "experience":"Fresher",
+            "salaryRange": "₹ 8 - 12 Lakhs",
+            "skilRequired": [
+                "UI/UX Design",
+                "Figma"
+            ],
+            "createdAt": 1712910640206,
+            "__v": 0,
+            "employeeEmail": "hr@gmail.com"
+        },
+        {
+            "_id": "6618f0e45502f224a4e5b54f",
+            "jobTitle": "Software Development Engineer",
+            "jobPoster": "https://c8.alamy.com/comp/2HA87X4/san-jose-ca-usa-october-17-2021-editorial-use-only-3d-cgi-adobe-signage-logo-on-top-of-glass-building-workplace-computer-software-company-in-h-2HA87X4.jpg",
+            "jobDescription": "Design, develop, and test software applications. Experience with object-oriented programming languages like Java or C++ and strong problem-solving skills.",
+            "employmentType": "Full Time",
+            "experience":"Fresher",
+            "location": "Noida, India",
+            "salaryRange": "₹ 15 - 20 Lakhs",
+            "skilRequired": [
+                "Backend",
+                "Java"
+            ],
+            "createdAt": 1712910564965,
+            "__v": 0,
+            "employeeEmail": "hr@gmail.com"
+        },
+        {
+            "_id": "6618ee495502f224a4e5b53e",
+            "jobTitle": "Machine Learning Engineer",
+            "jobPoster": "https://static2.bigstockphoto.com/6/7/4/large1500/476317187.jpg",
+            "jobDescription": "Develop and implement machine learning models to solve complex business problems. Experience with building and deploying large-scale production systems. Familiarity with deep learning frameworks like TensorFlow or PyTorch a plus.",
+            "employmentType": "Full Time",
+            "location": "Bangalore, India",
+            "salaryRange": "₹ 15 - 20 Lakhs",
+            "experience":"Fresher",
+            "skilRequired": [
+                "ML",
+                "Python"
+            ],
+            "createdAt": 1712909897944,
+            "__v": 0,
+            "employeeEmail": "hr@gmail.com"
+        },
+       
+               
+                {
+                    "_id": "6618ee8a5502f224a4e5b540",
+                    "jobTitle": "Front-End Developer",
+                    "jobPoster": "https://bsmedia.business-standard.com/_media/bs/img/article/2023-07/17/full/1689574606-2001.png",
+                    "jobDescription": "Build and maintain user-facing web applications. Experience with HTML, CSS, JavaScript frameworks like React or Angular. Strong understanding of responsive design principles.",
+                    "employmentType": "Contract",
+                    "experience":"Fresher",
+                    "location": "Gurgaon, India",
+                    "salaryRange": "₹ 8 - 12 Lakhs",
+                    "skilRequired": [
+                        "Frontend",
+                        "React"
+                    ],
+                    "createdAt": 1712909962093,
+                    "__v": 0,
+                    "employeeEmail": "hr@gmail.com"
+                },
+                {
+                    "_id": "6618eef55502f224a4e5b543",
+                    "jobTitle": "Full Stack Developer",
+                    "jobPoster": "https://businesstak.com/uploads/124509-2550718142.jpg",
+                    "jobDescription": "Develop and maintain both front-end and back-end web applications. Experience with a variety of technologies like HTML, CSS, JavaScript, Node.js, and databases like MySQL or PostgreSQL.",
+                    "employmentType": "Full Time",
+                    "experience":"Experience",
+                    "location": "Hyderabad, India",
+                    "salaryRange": "₹ 12 - 18 Lakhs",
+                    "skilRequired": [
+                        "Full Stack",
+                        "MERN Stack"
+                    ],
+                    "createdAt": 1712910069413,
+                    "__v": 0,
+                    "employeeEmail": "hr@gmail.com"
+                },
   ]);
   return (
     <>
@@ -43,9 +102,9 @@ const Applied = () => {
               <div className={JobStyle.company_box} key={index}>
                 <div className={JobStyle.saved_job_box}>
                   <div className={JobStyle.company_job_profile}>
-                    <img src={item.img} alt="comp_logo" />
+                    <img src={item.jobPoster} alt="comp_logo" />
                     <div className={JobStyle.job_position}>
-                      <h5>{item.jobrole}</h5>
+                      <h5>{item.jobTitle}</h5>
                       <p>12 hours ago</p>
                     </div>
                   </div>
@@ -54,7 +113,7 @@ const Applied = () => {
                       class="fa-solid fa-money-check-dollar"
                       style={{ color: "#067418" }}
                     ></i>
-                    <span style={{ fontWeight: "bold" }}>{item.lpa}K</span>
+                    <span style={{ fontWeight: "bold" }}>{item.salaryRange}K</span>
                     <span style={{ color: "#9595b4", fontSize: 12 }}>
                       /Month
                     </span>
@@ -79,7 +138,7 @@ const Applied = () => {
                         style={{ color: "black" }}
                       ></i>
                     </div>
-                    {item.category}
+                    {item.experience}
                   </div>
                   <div className={JobStyle.req_1}>
                     <div>
@@ -88,7 +147,7 @@ const Applied = () => {
                         style={{ color: "black" }}
                       ></i>
                     </div>
-                    fulltime
+                   {item.employmentType}
                   </div>
                 </div>
               </div>
--- frontend/src/pages/Job_Seeker/MyJobs/Saved.js
+++ frontend/src/pages/Job_Seeker/MyJobs/Saved.js
@@ -5,12 +5,61 @@ import { useNavigate } from "react-router-dom";
 const Saved = () => {
   const [saved_jobs] = useState([
     {
-      img: "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg",
-      jobrole: "Software Engineer",
-      lpa: 30,
-      location: "Indore",
-      category: "Fresher",
-    },
+                "_id": "6618ee495502f224a4e5b53e",
+                "jobTitle": "Machine Learning Engineer",
+                "jobPoster": "https://static2.bigstockphoto.com/6/7/4/large1500/476317187.jpg",
+                "jobDescription": "Develop and implement machine learning models to solve complex business problems. Experience with building and deploying large-scale production systems. Familiarity with deep learning frameworks like TensorFlow or PyTorch a plus.",
+                "employmentType": "Full Time",
+                "location": "Bangalore, India",
+                "salaryRange": "₹ 15 - 20 Lakhs",
+                "experience":"Fresher",
+                "skilRequired": [
+                    "ML",
+                    "Python"
+                ],
+                "createdAt": 1712909897944,
+                "__v": 0,
+                "employeeEmail": "hr@gmail.com"
+            },
+           
+                   
+                    {
+                        "_id": "6618ee8a5502f224a4e5b540",
+                        "jobTitle": "Front-End Developer",
+                        "jobPoster": "https://bsmedia.business-standard.com/_media/bs/img/article/2023-07/17/full/1689574606-2001.png",
+                        "jobDescription": "Build and maintain user-facing web applications. Experience with HTML, CSS, JavaScript frameworks like React or Angular. Strong understanding of responsive design principles.",
+                        "employmentType": "Contract",
+                        "experience":"Fresher",
+                        "location": "Gurgaon, India",
+                        "salaryRange": "₹ 8 - 12 Lakhs",
+                        "skilRequired": [
+                            "Frontend",
+                            "React"
+                        ],
+                        "createdAt": 1712909962093,
+                        "__v": 0,
+                        "employeeEmail": "hr@gmail.com"
+                    },
+                    {
+                        "_id": "6618eef55502f224a4e5b543",
+                        "jobTitle": "Full Stack Developer",
+                        "jobPoster": "https://businesstak.com/uploads/124509-2550718142.jpg",
+                        "jobDescription": "Develop and maintain both front-end and back-end web applications. Experience with a variety of technologies like HTML, CSS, JavaScript, Node.js, and databases like MySQL or PostgreSQL.",
+                        "employmentType": "Full Time",
+                        "experience":"Experience",
+                        "location": "Hyderabad, India",
+                        "salaryRange": "₹ 12 - 18 Lakhs",
+                        "skilRequired": [
+                            "Full Stack",
+                            "MERN Stack"
+                        ],
+                        "createdAt": 1712910069413,
+                        "__v": 0,
+                        "employeeEmail": "hr@gmail.com"
+                    },
+            
+            
+  
   ]);
   const navi = useNavigate();
 
@@ -23,9 +72,9 @@ const Saved = () => {
               <div className={JobStyle.company_box} key={index}>
                 <div className={JobStyle.saved_job_box}>
                   <div className={JobStyle.company_job_profile}>
-                    <img src={item.img} alt="comp_logo" />
+                    <img src={item.jobPoster} alt="comp_logo" />
                     <div className={JobStyle.job_position}>
-                      <h5>{item.jobrole}</h5>
+                      <h5>{item.jobTitle}</h5>
                       <p>12 hours ago</p>
                     </div>
                   </div>
@@ -34,7 +83,7 @@ const Saved = () => {
                       class="fa-solid fa-money-check-dollar"
                       style={{ color: "#067418" }}
                     ></i>
-                    <span style={{ fontWeight: "bold" }}>{item.lpa}K</span>
+                    <span style={{ fontWeight: "bold" }}>{item.salaryRange}</span>
                     <span style={{ color: "#9595b4", fontSize: 12 }}>
                       /Month
                     </span>
@@ -61,7 +110,7 @@ const Saved = () => {
                         style={{ color: "black" }}
                       ></i>
                     </div>
-                    {item.category}
+                    {item.experience}
                   </div>
                   <div className={JobStyle.req_1}>
                     <div>
@@ -70,7 +119,7 @@ const Saved = () => {
                         style={{ color: "black" }}
                       ></i>
                     </div>
-                    fulltime
+                    {item.employmentType}
                   </div>
                 </div>
               </div>
