7dca9e0654072a51226e8edb87c0e12264e5bee2
rani10245kumari <ranik10245@gmail.com>
2024-04-12 19:30:43 +0530

    applicationstatus

--- frontend/src/pages/Job_Seeker/MyResume/MyResume.js
+++ frontend/src/pages/Job_Seeker/MyResume/MyResume.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { GrNotes } from "react-icons/gr";
+import ResumeCss from './MyResume.module.css'
+import image from './img.png'
 
 function MyResume() {
   return (
     <div>
-      Resume page
+      <div className={ResumeCss.upperSelection}>
+        <p className={ResumeCss.button1}><GrNotes />My Resume</p>
+        <p className={ResumeCss.button1}><GrNotes />Previous Resume</p>
+      </div>
+
+      <button className={ResumeCss.Update_resumeBtn}>Update Your Resume</button>
+
+      <div className={ResumeCss.vertical_line}></div>
+      <div className={ResumeCss.right_side_container}>Upload your resume ATS friendly</div>
+      <img src={image} alt='#' className={ResumeCss.right_image}></img>
+      <div className={ResumeCss.right_side_container2}>Check your resume score to gauge its effectiveness and suitability for potential job opportunities</div>
+
+      <button className={ResumeCss.resume_btn}>Check your resume score</button>
+
     </div>
   )
 }
