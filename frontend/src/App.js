9a19ffeec259bc60f26eb5fd50f0425e5d4bccc5
Ayush5139 <2018.ayush.sachdev@ves.ac.in>
2024-04-20 16:01:35 +0530

    final

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
-
+import ApplicationStatus from "./pages/Job_Seeker/ApplicationStatus/ApplicationStatus";
 import AppRoute from "./Router/App.Route";
 import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <>
-      <Toaster position="top-center" reverseOrder={false} />
-      <AppRoute />
+      {/* <Toaster position="top-center" reverseOrder={false} />
+      <AppRoute /> */}
+      {/* <ApplicationStatus/> */}
+      <h1>fqWFqowf</h1>
     </>
   )
 }
