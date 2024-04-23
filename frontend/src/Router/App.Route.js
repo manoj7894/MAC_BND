5edcf965891c57c6aa99f63ebbefa9829c0e2679
Jerroshin <158449174+Webhosting123@users.noreply.github.com>
2024-04-23 14:49:17 +0530

    chatbotui

--- frontend/src/Router/App.Route.js
+++ frontend/src/Router/App.Route.js
@@ -4,38 +4,87 @@ import { Routes, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 //! These All Files are imported for the JobSeeker Routes
-const JobSeekerLayout = lazy(() => import("../pages/Job_Seeker/JobSeekerLayout"));
-const Dashboard = lazy(() => import("../pages/Job_Seeker/Dashboard/Dashboard.js"));
-const Assessment = lazy(() => import("../pages/Job_Seeker/Assessment/Assessment.js"));
-const InstructionPage = lazy(() => import("../pages/Job_Seeker/Assessment/InstructionPage.js"));
-const SelfAssessmentPage = lazy(() => import("../pages/Job_Seeker/Assessment/SelfAssessmentPage.js"));
-const AssessmentResult = lazy(() => import("../pages/Job_Seeker/Assessment/AssessmentResult.js"));
+const JobSeekerLayout = lazy(() =>
+  import("../pages/Job_Seeker/JobSeekerLayout")
+);
+const Dashboard = lazy(() =>
+  import("../pages/Job_Seeker/Dashboard/Dashboard.js")
+);
+const Assessment = lazy(() =>
+  import("../pages/Job_Seeker/Assessment/Assessment.js")
+);
+const InstructionPage = lazy(() =>
+  import("../pages/Job_Seeker/Assessment/InstructionPage.js")
+);
+const SelfAssessmentPage = lazy(() =>
+  import("../pages/Job_Seeker/Assessment/SelfAssessmentPage.js")
+);
+const AssessmentResult = lazy(() =>
+  import("../pages/Job_Seeker/Assessment/AssessmentResult.js")
+);
 const ChatBot = lazy(() => import("../pages/Job_Seeker/Chatbot/ChatBot.js"));
-const Analytics = lazy(() => import("../pages/Job_Seeker/Analytics/UserAnalytics.js"));
-const AnalyticsReportComponent = lazy(() => import("../pages/Job_Seeker/Analytics/UserAnalytics.js"));
+
+const Chatarea = lazy(() => import("../pages/Job_Seeker/Chatbot/Chatarea.js"));
+
+const Analytics = lazy(() =>
+  import("../pages/Job_Seeker/Analytics/UserAnalytics.js")
+);
+const AnalyticsReportComponent = lazy(() =>
+  import("../pages/Job_Seeker/Analytics/UserAnalytics.js")
+);
 const MYJobs = lazy(() => import("../pages/Job_Seeker/MyJobs/MyJobs.js"));
 const MYResume = lazy(() => import("../pages/Job_Seeker/MyResume/MyResume.js"));
-const Application = lazy(() => import("../pages/Job_Seeker/ApplicationStatus/ApplicationStatus.js"));
-const Interviews = lazy(() => import("../pages/Job_Seeker/InterviewScheduled/Interview.js"));
+const Application = lazy(() =>
+  import("../pages/Job_Seeker/ApplicationStatus/ApplicationStatus.js")
+);
+const Interviews = lazy(() =>
+  import("../pages/Job_Seeker/InterviewScheduled/Interview.js")
+);
 const Settings = lazy(() => import("../pages/Job_Seeker/Settings/Setting.js"));
-const JobListDetailedView = lazy(() =>import("../pages/Job_Seeker/Dashboard/DetailedView.js"));
-const Profile_details = lazy(()=>import("../pages/Job_Seeker/Dashboard/Profile_details.js"));
+const JobListDetailedView = lazy(() =>
+  import("../pages/Job_Seeker/Dashboard/DetailedView.js")
+);
+const Profile_details = lazy(() =>
+  import("../pages/Job_Seeker/Dashboard/Profile_details.js")
+);
 
 //! These All Files are imported for the Employer Routes
-const MainContainer = lazy(() => import("../pages/Employer/components/MainContainer.js"));
-const HRDashboard = lazy(() => import("../pages/Employer/Dashboard/HRDashboard.js"));
-const CreatePost = lazy(() => import("../pages/Employer/CreatePost/CreatePost.js"));
-const Employees = lazy(() => import("../pages/Employer/Employees/Employees.js"));
-const Attendance = lazy(() => import("../pages/Employer/Employees/Attendance.js"));
+const MainContainer = lazy(() =>
+  import("../pages/Employer/components/MainContainer.js")
+);
+const HRDashboard = lazy(() =>
+  import("../pages/Employer/Dashboard/HRDashboard.js")
+);
+const CreatePost = lazy(() =>
+  import("../pages/Employer/CreatePost/CreatePost.js")
+);
+const Employees = lazy(() =>
+  import("../pages/Employer/Employees/Employees.js")
+);
+const Attendance = lazy(() =>
+  import("../pages/Employer/Employees/Attendance.js")
+);
 const OnTime = lazy(() => import("../pages/Employer/Employees/OnTime.js"));
 const Absent = lazy(() => import("../pages/Employer/Employees/Absent.js"));
-const LateArrivals = lazy(() => import("../pages/Employer/Employees/LateArrivals.js"));
-const LeaveRequests = lazy(() => import("../pages/Employer/Employees/LeaveRequests.js"));
+const LateArrivals = lazy(() =>
+  import("../pages/Employer/Employees/LateArrivals.js")
+);
+const LeaveRequests = lazy(() =>
+  import("../pages/Employer/Employees/LeaveRequests.js")
+);
 const Payroll = lazy(() => import("../pages/Employer/Payroll/Payroll.js"));
-const HRAnalytics = lazy(() => import("../pages/Employer/Analytic/HrAnalytics.js"));
-const Candidates = lazy(() => import("../pages/Employer/Candidates/Candidates.js"));
-const InterviewScheduled = lazy(() => import("../pages/Employer/InterviewScheduled/InterviewScheduled.js"));
-const CreateInterview = lazy(() => import("../pages/Employer/InterviewScheduled/CreateInterview.js"));
+const HRAnalytics = lazy(() =>
+  import("../pages/Employer/Analytic/HrAnalytics.js")
+);
+const Candidates = lazy(() =>
+  import("../pages/Employer/Candidates/Candidates.js")
+);
+const InterviewScheduled = lazy(() =>
+  import("../pages/Employer/InterviewScheduled/InterviewScheduled.js")
+);
+const CreateInterview = lazy(() =>
+  import("../pages/Employer/InterviewScheduled/CreateInterview.js")
+);
 const Chatbot = lazy(() => import("../pages/Employer/ChatBot/Chatbot.js"));
 const Setting = lazy(() => import("../pages/Employer/Settings/Setting.js"));
 
@@ -45,7 +94,9 @@ const LoginPage = lazy(() =>
 );
 const Signup = lazy(() => import("../pages/Auth/Signup/UserSignup/UserSignup"));
 const HrLogin = lazy(() => import("../pages/Auth/Login/HrLogin/HrLogin"));
-const ResetPassword = lazy(() => import("../pages/Auth/Password/User/ResetPassword/ResetPassword.js"));
+const ResetPassword = lazy(() =>
+  import("../pages/Auth/Password/User/ResetPassword/ResetPassword.js")
+);
 const ForgotPassword = lazy(() =>
   import("../pages/Auth/Password/User/ForgotPassword/ForgotPassword.js")
 );
@@ -58,18 +109,15 @@ const HrForgotPassword = lazy(() =>
 //! These All Files are imported for the Auth Routes
 
 function AppRoute() {
-  const { userType } = useSelector(
-    (state) => state.Assessment.currentUser
-  );
+  const { userType } = useSelector((state) => state.Assessment.currentUser);
 
   return (
     <>
       {userType === "user" && <JobSeekerRoutes />}
-    
+
       {userType === "employee" && <EmployerRoutes />}
 
       {!userType && <AuthRouter />}
-
     </>
   );
 }
@@ -80,25 +128,161 @@ export default AppRoute;
 function JobSeekerRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Suspense><JobSeekerLayout /></Suspense>}>
-        <Route path="/dashboard" element={<Suspense><Dashboard /></Suspense>} />
-        <Route path="/assessment" element={<Suspense><Assessment /></Suspense>} />
-        <Route path="/chatbot" element={<Suspense><ChatBot /></Suspense>} />
-        <Route path="/analytics" element={<Suspense><Analytics /></Suspense>} />
-        <Route path="/analytics/weekly" element={<Suspense><AnalyticsReportComponent /></Suspense>} />
-        <Route path="/analytics/monthly" element={<Suspense><AnalyticsReportComponent /></Suspense>} />
-        <Route path="/analytics/yearly" element={<Suspense><AnalyticsReportComponent /></Suspense>} />
-        <Route path="/myjobs" element={<Suspense><MYJobs /></Suspense>} />
-        <Route path="/myresume" element={<Suspense><MYResume /></Suspense>} />
-        <Route path="/application" element={<Suspense><Application /></Suspense>} />
-        <Route path="/interviews" element={<Suspense><Interviews /></Suspense>} />
-        <Route path="/settings" element={<Suspense><Settings /></Suspense>} />
-        <Route path="/dashboard/:id" element={ <Suspense> <JobListDetailedView /> </Suspense> }/>
-        <Route path="/dashboard/profile_details" element={ <Suspense> <Profile_details /> </Suspense> }/>
+      <Route
+        path="/"
+        element={
+          <Suspense>
+            <JobSeekerLayout />
+          </Suspense>
+        }
+      >
+        <Route
+          path="/dashboard"
+          element={
+            <Suspense>
+              <Dashboard />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/assessment"
+          element={
+            <Suspense>
+              <Assessment />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/chatbot"
+          element={
+            <Suspense>
+              <ChatBot />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/Chatarea"
+          element={
+            <Suspense>
+              <Chatarea />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/analytics"
+          element={
+            <Suspense>
+              <Analytics />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/analytics/weekly"
+          element={
+            <Suspense>
+              <AnalyticsReportComponent />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/analytics/monthly"
+          element={
+            <Suspense>
+              <AnalyticsReportComponent />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/analytics/yearly"
+          element={
+            <Suspense>
+              <AnalyticsReportComponent />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/myjobs"
+          element={
+            <Suspense>
+              <MYJobs />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/myresume"
+          element={
+            <Suspense>
+              <MYResume />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/application"
+          element={
+            <Suspense>
+              <Application />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/interviews"
+          element={
+            <Suspense>
+              <Interviews />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/settings"
+          element={
+            <Suspense>
+              <Settings />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/dashboard/:id"
+          element={
+            <Suspense>
+              {" "}
+              <JobListDetailedView />{" "}
+            </Suspense>
+          }
+        />
+        <Route
+          path="/dashboard/profile_details"
+          element={
+            <Suspense>
+              {" "}
+              <Profile_details />{" "}
+            </Suspense>
+          }
+        />
       </Route>
-      <Route path="/assessment-Instructions" element={<Suspense><InstructionPage /></Suspense>} />
-      <Route path="/assessment-test" element={<Suspense><SelfAssessmentPage /></Suspense>} />
-      <Route path="/assessment-result" element={<Suspense><AssessmentResult /></Suspense>} />
+      <Route
+        path="/assessment-Instructions"
+        element={
+          <Suspense>
+            <InstructionPage />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/assessment-test"
+        element={
+          <Suspense>
+            <SelfAssessmentPage />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/assessment-result"
+        element={
+          <Suspense>
+            <AssessmentResult />
+          </Suspense>
+        }
+      />
     </Routes>
   );
 }
@@ -106,27 +290,146 @@ function JobSeekerRoutes() {
 function EmployerRoutes() {
   return (
     <Routes>
-      <Route path='/' element={<Suspense><MainContainer /></Suspense>}>
-        <Route path='/hr_dashboard' element={<Suspense><HRDashboard /></Suspense>} />
-        <Route path='/create_post' element={<Suspense><CreatePost /></Suspense>} />
-        <Route path='/employees' element={<Suspense><Employees /></Suspense>}>
-          <Route path='/employees/attendance' element={<Suspense><Attendance /></Suspense>} />
-          <Route path='/employees/on_time' element={<Suspense><OnTime /></Suspense>} />
-          <Route path='/employees/absent' element={<Suspense><Absent /></Suspense>} />
-          <Route path='/employees/late_arrivals' element={<Suspense><LateArrivals /></Suspense>} />
-          <Route path='/employees/leave_requests' element={<Suspense><LeaveRequests /></Suspense>} />
+      <Route
+        path="/"
+        element={
+          <Suspense>
+            <MainContainer />
+          </Suspense>
+        }
+      >
+        <Route
+          path="/hr_dashboard"
+          element={
+            <Suspense>
+              <HRDashboard />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/create_post"
+          element={
+            <Suspense>
+              <CreatePost />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/employees"
+          element={
+            <Suspense>
+              <Employees />
+            </Suspense>
+          }
+        >
+          <Route
+            path="/employees/attendance"
+            element={
+              <Suspense>
+                <Attendance />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/employees/on_time"
+            element={
+              <Suspense>
+                <OnTime />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/employees/absent"
+            element={
+              <Suspense>
+                <Absent />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/employees/late_arrivals"
+            element={
+              <Suspense>
+                <LateArrivals />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/employees/leave_requests"
+            element={
+              <Suspense>
+                <LeaveRequests />
+              </Suspense>
+            }
+          />
         </Route>
-        <Route path='/payroll' element={<Suspense><Payroll /></Suspense>} />
-        <Route path='/analytics' element={<Suspense><HRAnalytics /></Suspense>} />
-        <Route path='/candidates' element={<Suspense><Candidates /></Suspense>} />
-        <Route path='/interview_scheduled' element={<Suspense><InterviewScheduled /></Suspense>} />
-        <Route path='/schedule-interview' element={<Suspense><CreateInterview /></Suspense>} />
-        <Route path='/chatbot' element={<Suspense><Chatbot /></Suspense>} />
-        <Route path='/Setting' element={<Suspense><Setting /></Suspense>} />
+        <Route
+          path="/payroll"
+          element={
+            <Suspense>
+              <Payroll />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/analytics"
+          element={
+            <Suspense>
+              <HRAnalytics />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/candidates"
+          element={
+            <Suspense>
+              <Candidates />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/interview_scheduled"
+          element={
+            <Suspense>
+              <InterviewScheduled />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/schedule-interview"
+          element={
+            <Suspense>
+              <CreateInterview />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/chatbot"
+          element={
+            <Suspense>
+              <Chatbot />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/Setting"
+          element={
+            <Suspense>
+              <Setting />
+            </Suspense>
+          }
+        />
       </Route>
-      <Route path='/*' element={<Suspense><MainContainer /></Suspense>} />
+      <Route
+        path="/*"
+        element={
+          <Suspense>
+            <MainContainer />
+          </Suspense>
+        }
+      />
     </Routes>
-  )
+  );
 }
 
 // All Routing related to the Auth must be defined HERE
--- frontend/src/pages/Job_Seeker/Chatbot/ChatBot.js
+++ frontend/src/pages/Job_Seeker/Chatbot/ChatBot.js
@@ -1,11 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import ChatBotStyle from "./Chatbot.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import icon2 from "./image/icon2.png";
+import bg from "../../../Assets/backdropimage.png";
+
+import { FaArrowRightLong } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 function ChatBot() {
+  const [showImage, setShowImage] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowImage(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div>
-      Chat BOT
+      <div className={ChatBotStyle.chatbot_full}>
+        {showImage && (
+          <div className={ChatBotStyle.chatbot_full_image}>
+            <img src={bg} alt={"bg"} className={ChatBotStyle.chatbot_bg} />
+          </div>
+        )}
+        <Container>
+          <Row>
+            <Col xxl={12}>
+              <div className={ChatBotStyle.chatbot_full_content}>
+                <div className={ChatBotStyle.chatbot_title}>
+                  Your AI Assistant
+                </div>
+                <div className={ChatBotStyle.chatbot_content}>
+                  Using this software, you can ask <br /> questions and receive
+                  articles using <br /> an artificial intelligence assistant
+                </div>
+                <div className={ChatBotStyle.chatbot_content_image}>
+                  <img
+                    src={icon2}
+                    alt={"icon2"}
+                    className={ChatBotStyle.chatbot_image}
+                  />
+                </div>
+
+                <Link to="/Chatarea" className={ChatBotStyle.chatbot_link}>
+                  <button className={ChatBotStyle.chatbot_button}>
+                    Continue
+                    <span>
+                      <FaArrowRightLong
+                        className={ChatBotStyle.chatbot_button_arrow}
+                      />
+                    </span>
+                  </button>
+                </Link>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ChatBot
+export default ChatBot;
