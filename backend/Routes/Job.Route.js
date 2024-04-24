e65a2a9903477c691c6164f7266bf5e7502b1ae7
Altamash9146 <sayyedaltamash853@gmail.com>
2024-04-24 18:35:54 +0530

    create post full complete

--- backend/Routes/Job.Route.js
+++ backend/Routes/Job.Route.js
@@ -1,10 +1,19 @@
-const { uploadPhoto } = require("../middleware/fileUploadMiddleware")
+const { uploadPhoto } = require("../middleware/fileUploadMiddleware");
 
 const jobRoutes = require("express").Router();
 
 // uploadPhoto()
 
-const { create, get, update, remove, getAll, getJobByID, } = require("../controller/Job.controller");
+const {
+  create,
+  get,
+  update,
+  remove,
+  getAll,
+  getJobByID,
+} = require("../controller/Job.controller");
+
+const mcqController = require("../controller/InterviewSchedule/AptitudeRoundController");
 
 jobRoutes.post("/create-job", uploadPhoto, create);
 
@@ -17,5 +26,16 @@ jobRoutes.get("/All-jobs", getAll);
 jobRoutes.put("/update-job/:id", update);
 
 jobRoutes.delete("/delete-job/:id", remove);
+
+jobRoutes.get('/get-mcq', mcqController.getAllMCQs);
+
+// Route to create a new MCQ
+jobRoutes.post('/create', mcqController.createMCQ);
+
+// Route to update an existing MCQ
+jobRoutes.patch('/:id', mcqController.updateMCQ);
+
+// Route to delete an MCQ
+jobRoutes.delete('/:id', mcqController.deleteMCQ);
 
 module.exports = jobRoutes;
