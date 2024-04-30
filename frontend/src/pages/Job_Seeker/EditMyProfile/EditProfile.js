394176362022f10b915e750f31e88a93f497ddd5
Anshuman Kumar <mahianshuman491@gmail.com>
2024-04-30 12:28:12 +0530

    add the tag input for skils list

--- frontend/src/pages/Job_Seeker/EditMyProfile/EditProfile.js
+++ frontend/src/pages/Job_Seeker/EditMyProfile/EditProfile.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { useSelector } from "react-redux";
 import Loader from "../../Common-Components/Loaders/Loader";
+import { RxCross2 } from "react-icons/rx";
 const baseURL = process.env.REACT_APP_BACKEND_BASE_URL;
 
 function EditProfile() {
@@ -38,6 +39,10 @@ function EditProfile() {
     marital_status: "",
   });
 
+
+
+  // ! Onchange event for form input
+
   //! Load user data by using his email address
   const loadUserData = () => {
     setIsLoading(true);
@@ -57,7 +62,6 @@ function EditProfile() {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(loadUserData, [email]);
 
-  console.log(userDetails);
   return (
     <main className={styleSheet.mainContainer}>
       {IsLoading ? (
@@ -68,43 +72,43 @@ function EditProfile() {
           <div className={styleSheet.Form__profileContainer}>
             <label htmlFor="profilePicture" className={styleSheet.form__userLabel}>Profile Picture</label>
             <div className={styleSheet.Form__profileBox}>
-              <img src={userDetails?.profileImage} alt={`${name}-Profile`} className={styleSheet.Form__userProfile}/>
+              <img src={userDetails?.profileImage} alt={`${name}-Profile`} className={styleSheet.Form__userProfile} />
             </div>
           </div>
 
           <div className={`${styleSheet.Form__inputRows_Primary}`}>
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="firstName" className={styleSheet.Form__inputBox_Label}> First Name</label>
-              <input type="text" id="firstName" placeholder="Enter your first name" className={styleSheet.Form__input} />
+              <input type="text" value={userDetails.name?.split(" ")[0]} name="firstName" id="firstName" placeholder="Enter your first name" className={styleSheet.Form__input} />
             </div>
 
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="lastName" className={styleSheet.Form__inputBox_Label}>Last Name</label>
-              <input type="text" id="lastName" placeholder="Enter your last name" className={styleSheet.Form__input} />
+              <input type="text" name="lastName" value={userDetails.name?.split(" ")[1]} id="lastName" placeholder="Enter your last name" className={styleSheet.Form__input}  />
             </div>
           </div>
 
           <div className={`${styleSheet.Form__inputRows_Secondry}`}>
             <label htmlFor="email" className={styleSheet.Form__inputBox_Label}> Email Address</label>
-            <input type="email" name="email" id="email" placeholder="Enter your email address" className={styleSheet.Form__input} />
+            <input type="email" value={userDetails.email} name="email" id="email" placeholder="Enter your email address" className={styleSheet.Form__input} />
           </div>
 
           <div className={`${styleSheet.Form__inputRows_Secondry}`}>
             <label htmlFor="phone" className={styleSheet.Form__inputBox_Label}>Phone</label>
-            <input type="phone" name="phone" id="phone" placeholder="Enter your phone number" className={styleSheet.Form__input} />
+            <input type="phone" value={userDetails.phone_number} name="phone" id="phone" placeholder="Enter your phone number" className={styleSheet.Form__input} />
           </div>
 
-          
+
           <div className={`${styleSheet.Form__inputRows_Secondry}`}>
             <label htmlFor="website" className={styleSheet.Form__inputBox_Label}>Website</label>
-            <input type="text" name="website" id="website" placeholder="Enter your website link" className={styleSheet.Form__input} />
+            <input type="text" value={userDetails.website} name="website" id="website" placeholder="Enter your website link" className={styleSheet.Form__input} />
           </div>
 
           <div className={`${styleSheet.Form__inputRows_Primary}`}>
 
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="maritalStatus" className={styleSheet.Form__inputBox_Label}> Marital Status</label>
-              <select className={`${styleSheet.Form__input} ${styleSheet.Form__Select_input}`}>
+              <select value={userDetails.marital_status} className={`${styleSheet.Form__input} ${styleSheet.Form__Select_input}`}>
                 <option value="">Select your marital status</option>
                 <option value="single" className={styleSheet.Form_SelectInput_options}>single</option>
                 <option value="married" className={styleSheet.Form_SelectInput_options}>married</option>
@@ -116,50 +120,59 @@ function EditProfile() {
 
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="dob" className={styleSheet.Form__inputBox_Label}>Date of birth</label>
-              <input type="date" name="dob" id="dob" className={styleSheet.Form__input} />
+              <input type="date" name="dob" id="dob" value={userDetails.dob?.split("T")[0]} className={styleSheet.Form__input} />
             </div>
           </div>
 
           <div className={`${styleSheet.Form__inputRows_Primary}`}>
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="gender" className={styleSheet.Form__inputBox_Label}> Gender</label>
-              <select className={`${styleSheet.Form__input} ${styleSheet.Form__Select_input}`}>
+              <select value={userDetails.gender} className={`${styleSheet.Form__input} ${styleSheet.Form__Select_input}`}>
                 <option value="">Select your gender</option>
-                <option value="male" className={styleSheet.Form_SelectInput_options}>male</option>
-                <option value="female" className={styleSheet.Form_SelectInput_options}>female</option>
-                <option value="other" className={styleSheet.Form_SelectInput_options}>other</option>
+                <option value="Male" className={styleSheet.Form_SelectInput_options}>male</option>
+                <option value="Female" className={styleSheet.Form_SelectInput_options}>female</option>
+                <option value="Other" className={styleSheet.Form_SelectInput_options}>other</option>
               </select>
             </div>
 
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="nationality" className={styleSheet.Form__inputBox_Label}> nationality</label>
-              <select className={`${styleSheet.Form__input} ${styleSheet.Form__Select_input}`}>
+              <select value={userDetails.country} className={`${styleSheet.Form__input} ${styleSheet.Form__Select_input}`}>
                 <option value="">Select your nationality</option>
-                <option value="indian" className={styleSheet.Form_SelectInput_options}>indian</option>
-                <option value="non-indian" className={styleSheet.Form_SelectInput_options}>non-indian</option>
+                <option value="Indian" className={styleSheet.Form_SelectInput_options}>indian</option>
+                <option value="Non-Indian" className={styleSheet.Form_SelectInput_options}>non-indian</option>
               </select>
             </div>
           </div>
-          
+
           <div className={`${styleSheet.Form__inputRows_Primary}`}>
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="college" className={styleSheet.Form__inputBox_Label}> university/College</label>
-              <input type="text" id="college" placeholder="Enter College or university name" className={styleSheet.Form__input} />
+              <input type="text" value={userDetails.college} id="college" placeholder="Enter College or university name" className={styleSheet.Form__input} />
             </div>
 
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="course" className={styleSheet.Form__inputBox_Label}>course</label>
-              <input type="text" id="course" placeholder="Enter course name" className={styleSheet.Form__input} />
+              <input type="text" value={userDetails.course} id="course" placeholder="Enter course name" className={styleSheet.Form__input} />
             </div>
           </div>
 
           <div className={`${styleSheet.Form__inputRows_Secondry}`}>
             <label htmlFor="percentage" className={styleSheet.Form__inputBox_Label}> percentage</label>
-            <input type="number" inputmode="numeric" name="percentage" id="percentage" placeholder="Enter your percentage" className={styleSheet.Form__input} />
+            <input type="number" value={userDetails.percentage} name="percentage" id="percentage" placeholder="Enter your percentage" className={styleSheet.Form__input} />
           </div>
 
           <div className={`${styleSheet.Form__inputRows_Secondry}`}>
             <label htmlFor="skills" className={styleSheet.Form__inputBox_Label}> Skills</label>
+            {
+              userDetails?.skills?.length > 0 && <ul className={styleSheet.Form_skilsList}>
+                {
+                  userDetails?.skills?.map((skils, index) => {
+                    return <li key={index} className={styleSheet.Form_skilsList_Item}>{skils?.name} <RxCross2 className={styleSheet.Form_skilsList_Item_DeleteButton} /> </li>
+                  })
+                }
+              </ul>
+            }
             <input type="text" name="skills" id="skills" placeholder="Enter your skils" className={styleSheet.Form__input} />
           </div>
 
@@ -169,30 +182,30 @@ function EditProfile() {
           <div className={`${styleSheet.Form__inputRows_Primary}`}>
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="jobTitle" className={styleSheet.Form__inputBox_Label}> Job Title</label>
-              <input type="text" id="jobTitle" placeholder="Enter job title" className={styleSheet.Form__input} />
+              <input type="text" value={userDetails.job_title} id="jobTitle" placeholder="Enter job title" className={styleSheet.Form__input} />
             </div>
 
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="companyName" className={styleSheet.Form__inputBox_Label}>Company Name</label>
-              <input type="text" id="companyName" placeholder="Enter company name" className={styleSheet.Form__input} />
+              <input type="text" value={userDetails.company} id="companyName" placeholder="Enter company name" className={styleSheet.Form__input} />
             </div>
           </div>
 
           <div className={`${styleSheet.Form__inputRows_Primary}`}>
             <div className={styleSheet.Form__inputBox}>
-              <label htmlFor="college" className={styleSheet.Form__inputBox_Label}> university/College</label>
-              <input type="text" id="college" placeholder="Enter College or university name" className={styleSheet.Form__input} />
+              <label htmlFor="company_start_date" className={styleSheet.Form__inputBox_Label}>Start Date</label>
+              <input type="date" name="company_start_date" id="company_start_date" value={userDetails.company_start_date?.split("T")[0]} className={styleSheet.Form__input} />
             </div>
 
             <div className={styleSheet.Form__inputBox}>
-              <label htmlFor="course" className={styleSheet.Form__inputBox_Label}>course</label>
-              <input type="text" id="course" placeholder="Enter course name" className={styleSheet.Form__input} />
+              <label htmlFor="company_end_date" className={styleSheet.Form__inputBox_Label}>End Date</label>
+              <input type="date" name="company_end_date" id="company_end_date" value={userDetails.company_end_date?.split("T")[0]} className={styleSheet.Form__input} />
             </div>
           </div>
 
-          <div className={`${styleSheet.Form__inputRows_Secondry}`} style={{"width": "100%"}}>
+          <div className={`${styleSheet.Form__inputRows_Secondry}`} style={{ "width": "100%" }}>
             <label htmlFor="about" className={styleSheet.Form__inputBox_Label}> About</label>
-            <textarea className={styleSheet.Form_About_textArea} placeholder="Write about yourself" id="about" ></textarea>
+            <textarea className={styleSheet.Form_About_textArea} placeholder="Write about yourself" id="about" value={userDetails?.biography} ></textarea>
           </div>
 
           <div className={styleSheet.Form__buttonContainer}>
