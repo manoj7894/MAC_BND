dd294b85e6a575afc3cc669919e686a61f9e920e
Anshuman Kumar <mahianshuman491@gmail.com>
2024-04-30 13:08:16 +0530

    add onchange event in all the input fields othen then Skils input fields

--- frontend/src/pages/Job_Seeker/EditMyProfile/EditProfile.js
+++ frontend/src/pages/Job_Seeker/EditMyProfile/EditProfile.js
@@ -42,7 +42,21 @@ function EditProfile() {
 
 
   // ! Onchange event for form input
+  const handleOnChange = (e) => {
+    let { name, value } = e.target;
+    setUserDetails({ ...userDetails, [name]: value });
 
+    // Combine first name and last name into one variable "name"
+    if (name === "firstName" || name === "lastName") {
+      const firstName = name === "firstName" ? value : userDetails.name.split(" ")[0];
+      const lastName = name === "lastName" ? value : (userDetails.name?.split(" ")[1] ?? "");
+      const fullName = `${firstName} ${lastName}`;
+      setUserDetails({ ...userDetails, name: fullName });
+    }
+
+  };
+
+  console.log(userDetails)
   //! Load user data by using his email address
   const loadUserData = () => {
     setIsLoading(true);
@@ -76,39 +90,43 @@ function EditProfile() {
             </div>
           </div>
 
+          {/* First name and last name */}
           <div className={`${styleSheet.Form__inputRows_Primary}`}>
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="firstName" className={styleSheet.Form__inputBox_Label}> First Name</label>
-              <input type="text" value={userDetails.name?.split(" ")[0]} name="firstName" id="firstName" placeholder="Enter your first name" className={styleSheet.Form__input} />
+              <input type="text" value={userDetails.name?.split(" ")[0]} name="firstName" id="firstName" placeholder="Enter your first name" className={styleSheet.Form__input} onChange={handleOnChange} />
             </div>
 
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="lastName" className={styleSheet.Form__inputBox_Label}>Last Name</label>
-              <input type="text" name="lastName" value={userDetails.name?.split(" ")[1]} id="lastName" placeholder="Enter your last name" className={styleSheet.Form__input}  />
+              <input type="text" name="lastName" value={userDetails.name?.split(" ")[1]} id="lastName" placeholder="Enter your last name" className={styleSheet.Form__input} onChange={handleOnChange} />
             </div>
           </div>
 
+          {/* Email address */}
           <div className={`${styleSheet.Form__inputRows_Secondry}`}>
             <label htmlFor="email" className={styleSheet.Form__inputBox_Label}> Email Address</label>
-            <input type="email" value={userDetails.email} name="email" id="email" placeholder="Enter your email address" className={styleSheet.Form__input} />
+            <input type="email" value={userDetails.email} name="email" id="email" placeholder="Enter your email address" className={styleSheet.Form__input} onChange={handleOnChange} />
           </div>
 
+          {/* Phone number */}
           <div className={`${styleSheet.Form__inputRows_Secondry}`}>
             <label htmlFor="phone" className={styleSheet.Form__inputBox_Label}>Phone</label>
-            <input type="phone" value={userDetails.phone_number} name="phone" id="phone" placeholder="Enter your phone number" className={styleSheet.Form__input} />
+            <input type='tel' pattern="[0-9]{10}" value={userDetails.phone_number} name="phone_number" id="phone" placeholder="Enter your phone number" className={styleSheet.Form__input} onChange={handleOnChange} maxLength="10" />
           </div>
 
-
+          {/* WEbsites*/}
           <div className={`${styleSheet.Form__inputRows_Secondry}`}>
             <label htmlFor="website" className={styleSheet.Form__inputBox_Label}>Website</label>
-            <input type="text" value={userDetails.website} name="website" id="website" placeholder="Enter your website link" className={styleSheet.Form__input} />
+            <input type="text" value={userDetails.website} name="website" id="website" placeholder="Enter your website link" className={styleSheet.Form__input} onChange={handleOnChange} />
           </div>
 
+          {/* marital status and Date of birth */}
           <div className={`${styleSheet.Form__inputRows_Primary}`}>
 
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="maritalStatus" className={styleSheet.Form__inputBox_Label}> Marital Status</label>
-              <select value={userDetails.marital_status} className={`${styleSheet.Form__input} ${styleSheet.Form__Select_input}`}>
+              <select value={userDetails.marital_status} className={`${styleSheet.Form__input} ${styleSheet.Form__Select_input}`} name="marital_status" onChange={handleOnChange}>
                 <option value="">Select your marital status</option>
                 <option value="single" className={styleSheet.Form_SelectInput_options}>single</option>
                 <option value="married" className={styleSheet.Form_SelectInput_options}>married</option>
@@ -120,14 +138,15 @@ function EditProfile() {
 
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="dob" className={styleSheet.Form__inputBox_Label}>Date of birth</label>
-              <input type="date" name="dob" id="dob" value={userDetails.dob?.split("T")[0]} className={styleSheet.Form__input} />
+              <input type="date" name="dob" id="dob" value={userDetails.dob?.split("T")[0]} className={styleSheet.Form__input} onChange={handleOnChange} />
             </div>
           </div>
 
+          {/* Gender and nationality */}
           <div className={`${styleSheet.Form__inputRows_Primary}`}>
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="gender" className={styleSheet.Form__inputBox_Label}> Gender</label>
-              <select value={userDetails.gender} className={`${styleSheet.Form__input} ${styleSheet.Form__Select_input}`}>
+              <select value={userDetails.gender} className={`${styleSheet.Form__input} ${styleSheet.Form__Select_input}`} onChange={handleOnChange} name="gender">
                 <option value="">Select your gender</option>
                 <option value="Male" className={styleSheet.Form_SelectInput_options}>male</option>
                 <option value="Female" className={styleSheet.Form_SelectInput_options}>female</option>
@@ -137,7 +156,7 @@ function EditProfile() {
 
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="nationality" className={styleSheet.Form__inputBox_Label}> nationality</label>
-              <select value={userDetails.country} className={`${styleSheet.Form__input} ${styleSheet.Form__Select_input}`}>
+              <select value={userDetails.country} name="country" className={`${styleSheet.Form__input} ${styleSheet.Form__Select_input}`} onChange={handleOnChange}>
                 <option value="">Select your nationality</option>
                 <option value="Indian" className={styleSheet.Form_SelectInput_options}>indian</option>
                 <option value="Non-Indian" className={styleSheet.Form_SelectInput_options}>non-indian</option>
@@ -148,18 +167,18 @@ function EditProfile() {
           <div className={`${styleSheet.Form__inputRows_Primary}`}>
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="college" className={styleSheet.Form__inputBox_Label}> university/College</label>
-              <input type="text" value={userDetails.college} id="college" placeholder="Enter College or university name" className={styleSheet.Form__input} />
+              <input type="text" name="college" value={userDetails.college} id="college" placeholder="Enter College or university name" className={styleSheet.Form__input} onChange={handleOnChange} />
             </div>
 
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="course" className={styleSheet.Form__inputBox_Label}>course</label>
-              <input type="text" value={userDetails.course} id="course" placeholder="Enter course name" className={styleSheet.Form__input} />
+              <input type="text" name="course" value={userDetails.course} id="course" placeholder="Enter course name" className={styleSheet.Form__input} onChange={handleOnChange} />
             </div>
           </div>
 
           <div className={`${styleSheet.Form__inputRows_Secondry}`}>
             <label htmlFor="percentage" className={styleSheet.Form__inputBox_Label}> percentage</label>
-            <input type="number" value={userDetails.percentage} name="percentage" id="percentage" placeholder="Enter your percentage" className={styleSheet.Form__input} />
+            <input type="number" value={userDetails.percentage} name="percentage" id="percentage" placeholder="Enter your percentage" className={styleSheet.Form__input} onChange={handleOnChange} />
           </div>
 
           <div className={`${styleSheet.Form__inputRows_Secondry}`}>
@@ -182,30 +201,30 @@ function EditProfile() {
           <div className={`${styleSheet.Form__inputRows_Primary}`}>
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="jobTitle" className={styleSheet.Form__inputBox_Label}> Job Title</label>
-              <input type="text" value={userDetails.job_title} id="jobTitle" placeholder="Enter job title" className={styleSheet.Form__input} />
+              <input type="text" name="job_title" value={userDetails.job_title ?? ""} id="jobTitle" placeholder="Enter job title" className={styleSheet.Form__input} onChange={handleOnChange} />
             </div>
 
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="companyName" className={styleSheet.Form__inputBox_Label}>Company Name</label>
-              <input type="text" value={userDetails.company} id="companyName" placeholder="Enter company name" className={styleSheet.Form__input} />
+              <input type="text" name="company" value={userDetails.company ?? ""} id="companyName" placeholder="Enter company name" className={styleSheet.Form__input} onChange={handleOnChange} />
             </div>
           </div>
 
           <div className={`${styleSheet.Form__inputRows_Primary}`}>
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="company_start_date" className={styleSheet.Form__inputBox_Label}>Start Date</label>
-              <input type="date" name="company_start_date" id="company_start_date" value={userDetails.company_start_date?.split("T")[0]} className={styleSheet.Form__input} />
+              <input type="date" name="company_start_date" id="company_start_date" value={userDetails.company_start_date?.split("T")[0] ?? ""} className={styleSheet.Form__input} onChange={handleOnChange} />
             </div>
 
             <div className={styleSheet.Form__inputBox}>
               <label htmlFor="company_end_date" className={styleSheet.Form__inputBox_Label}>End Date</label>
-              <input type="date" name="company_end_date" id="company_end_date" value={userDetails.company_end_date?.split("T")[0]} className={styleSheet.Form__input} />
+              <input type="date" name="company_end_date" id="company_end_date" value={userDetails.company_end_date?.split("T")[0] ?? ""} className={styleSheet.Form__input} onChange={handleOnChange} />
             </div>
           </div>
 
           <div className={`${styleSheet.Form__inputRows_Secondry}`} style={{ "width": "100%" }}>
             <label htmlFor="about" className={styleSheet.Form__inputBox_Label}> About</label>
-            <textarea className={styleSheet.Form_About_textArea} placeholder="Write about yourself" id="about" value={userDetails?.biography} ></textarea>
+            <textarea className={styleSheet.Form_About_textArea} placeholder="Write about yourself" id="about" name="biography" value={userDetails?.biography} onChange={handleOnChange}></textarea>
           </div>
 
           <div className={styleSheet.Form__buttonContainer}>
