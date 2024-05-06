74ba366b27e641e6667503c4366c2e002a4eded9
Anshuman Kumar <mahianshuman491@gmail.com>
2024-05-06 22:57:14 +0530

    add the application view backend feature

--- backend/Routes/MyJob.Route.js
+++ backend/Routes/MyJob.Route.js
@@ -1,18 +1,19 @@
 
 const myJobRoutes = require("express").Router();
-const { createAppliedJob, getAppliedJob, removeAppliedJob, createSavedJob, getSavedJob, removeSavedJob, getApplicantsForJob } = require("../controller/MyJob.contoller")
+const { createAppliedJob, getAppliedJob, removeAppliedJob, createSavedJob, getSavedJob, removeSavedJob, getApplicantsForJob, updateApplicationStatus } = require("../controller/MyJob.contoller")
 
 myJobRoutes.post("/create/apply-job", createAppliedJob);
 myJobRoutes.get("/get/apply-job/:email", getAppliedJob);
 myJobRoutes.delete("/delete/apply-job/:email", removeAppliedJob);
 myJobRoutes.get('/job/:jobId/applicants', getApplicantsForJob);
+myJobRoutes.patch('/applicationStatus/:email', updateApplicationStatus);
 
 
 
 
-myJobRoutes.post("/create/save-job",createSavedJob);
-myJobRoutes.get("/get/save-job/:email",getSavedJob);
-myJobRoutes.delete("/delete/save-job/:email",removeSavedJob);
+myJobRoutes.post("/create/save-job", createSavedJob);
+myJobRoutes.get("/get/save-job/:email", getSavedJob);
+myJobRoutes.delete("/delete/save-job/:email", removeSavedJob);
 
 
 module.exports = myJobRoutes;
