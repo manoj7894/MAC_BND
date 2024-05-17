66b15624702a96d7e586023b8bc401b97e682559
Anshuman Kumar <mahianshuman491@gmail.com>
2024-05-17 15:39:52 +0530

    add the earch functionality in hr dashboard

--- frontend/src/pages/Employer/Dashboard/HRDashboard.js
+++ frontend/src/pages/Employer/Dashboard/HRDashboard.js
@@ -1,42 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import hrdashboard from './HrDashboard.module.css'
-import user from '../../../Assets/user.png';
+import React, { useEffect, useState } from "react";
+import hrdashboard from "./HrDashboard.module.css";
+import user from "../../../Assets/user.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowUpRightFromSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import { faArrowUpRightFromSquare, faTrash,} from "@fortawesome/free-solid-svg-icons";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import axios from "axios";
-import { format } from 'date-fns';
-import Loader from '../../Common-Components/Loaders/Loader';
-import { CalculateTimeAgo } from '../../Common-Components/TimeAgo';
-import HrJobDetail from './HrJobDetail';
+import { format } from "date-fns";
+import Loader from "../../Common-Components/Loaders/Loader";
+import { CalculateTimeAgo } from "../../Common-Components/TimeAgo";
+import HrJobDetail from "./HrJobDetail";
 import toast from "react-hot-toast";
+import { useSelector } from "react-redux";
 
 const responsive = {
   superLargeDesktop: { breakpoint: { max: 4000, min: 3000 }, items: 4 },
   desktop: { breakpoint: { max: 3000, min: 1024 }, items: 3 },
   tablet: { breakpoint: { max: 1024, min: 464 }, items: 2 },
-  mobile: { breakpoint: { max: 464, min: 0 }, items: 1 }
+  mobile: { breakpoint: { max: 464, min: 0 }, items: 1 },
 };
 
 export default function HRDashboard() {
+  const { SearchOptions } = useSelector((state) => state.Filter);
   const [jobPost, setJobPost] = useState([]);
   const [sortedJob, setSortedJob] = useState([]);
-  const [selectedSort, setSelectedSort] = useState('Sort By');
+  const [selectedSort, setSelectedSort] = useState("Sort By");
   const [loading, setLoading] = useState(false);
   const [selectedJobId, setSelectedJobId] = useState(null);
-  const [ShowApplicantDetails, setShowApplicantDetails] = useState(false)
+  const [ShowApplicantDetails, setShowApplicantDetails] = useState(false);
 
   const formattedDate = (timestamp) => {
     if (!timestamp) {
-      return 'N/A';
+      return "N/A";
     }
-    return format(new Date(timestamp), 'do MMMM, yyyy');
+    return format(new Date(timestamp), "do MMMM, yyyy");
   };
 
   const loadJobPost = () => {
     setLoading(true);
-    axios.get(`http://localhost:8080/api/jobs/get-job/${localStorage.getItem("email")}`)
+    axios
+      .get(
+        `http://localhost:8080/api/jobs/get-job/${localStorage.getItem(
+          "email"
+        )}`
+      )
       .then((response) => {
         setJobPost(response.data.jobs);
         setSortedJob(
@@ -52,13 +59,13 @@ export default function HRDashboard() {
     const sorted = [...jobPost];
     setSortedJob(sorted);
     switch (e.target.value) {
-      case 'latest':
+      case "latest":
         sorted.sort((a, b) => b.createdAt - a.createdAt);
         break;
-      case 'name':
+      case "name":
         sorted.sort((a, b) => a.jobTitle.localeCompare(b.jobTitle));
         break;
-      case 'city':
+      case "city":
         sorted.sort((a, b) => a.location.localeCompare(b.location));
         break;
       default:
@@ -67,7 +74,7 @@ export default function HRDashboard() {
   };
 
   const handleJobCardClick = (jobId) => {
-    setShowApplicantDetails(false)
+    setShowApplicantDetails(false);
     // If the clicked card is already selected, toggle to show latest posts
     if (selectedJobId === jobId) {
       setSelectedJobId(null); // Reset selectedJobId to show latest posts
@@ -78,7 +85,8 @@ export default function HRDashboard() {
 
   const handleDelete = async (postID) => {
     try {
-      await axios.delete(`http://localhost:8080/api/jobs/delete-job/${postID}`)
+      await axios
+        .delete(`http://localhost:8080/api/jobs/delete-job/${postID}`)
         .then((response) => {
           if (response.data.success) {
             toast.success(`Job deleted successfully`);
@@ -91,15 +99,49 @@ export default function HRDashboard() {
     }
   };
 
+  useEffect(() => {
+    if (SearchOptions.searchText) {
+      let FilteredData = jobPost.filter(
+        (job) =>
+          job.jobTitle
+            .toLowerCase()
+            .includes(SearchOptions.searchText.trim().toLowerCase()) ||
+          job.employmentType
+            .toLowerCase()
+            .includes(SearchOptions.searchText.trim().toLowerCase()) ||
+          job?.skilRequired?.some((skil) =>
+            skil.name
+              .toLowerCase()
+              .includes(SearchOptions.searchText.trim().toLowerCase())
+          ) ||
+          job?.location
+            .toLowerCase()
+            .includes(SearchOptions.searchText.trim().toLowerCase())
+      );
+      setSortedJob(FilteredData);
+    } else {
+      setSortedJob(jobPost);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [SearchOptions.searchText]);
+
   return (
     <div className={hrdashboard.__dashboard_Page}>
-      {loading ? <Loader /> : (
+      {loading ? (
+        <Loader />
+      ) : (
         <>
           <header className={hrdashboard.__dashboard_Header}>
-            <h2 style={{ fontWeight: '700' }}>Dashboard</h2>
+            <h2 style={{ fontWeight: "700" }}>Dashboard</h2>
             <div className={hrdashboard.__dropdown}>
-              <select className={hrdashboard.selectOption} value={selectedSort} onChange={handleSortBy}>
-                <option className={hrdashboard.options} value="">Sort By</option>
+              <select
+                className={hrdashboard.selectOption}
+                value={selectedSort}
+                onChange={handleSortBy}
+              >
+                <option className={hrdashboard.options} value="">
+                  Sort By
+                </option>
                 <option value="latest">Latest</option>
                 <option value="name">Name</option>
                 <option value="city">City</option>
@@ -110,9 +152,10 @@ export default function HRDashboard() {
           <div className={hrdashboard.__post_Impression}>
             <header className={hrdashboard.__postImpression_Header}>
               <h3>Post Impressions</h3>
-              <p className=''>see all</p>
+              <p className="">see all</p>
             </header>
-            <Carousel className={hrdashboard.__post_Section}
+            <Carousel
+              className={hrdashboard.__post_Section}
               swipeable={true}
               draggable={true}
               showDots={false}
@@ -126,34 +169,57 @@ export default function HRDashboard() {
               containerClass="carousel-container"
               removeArrowOnDeviceType={["tablet", "mobile", "desktop"]}
               dotListClass="custom-dot-list-style"
-              itemClass="carousel-item-padding-40-px">
-              {jobPost.length > 0 && jobPost.map((data) => (
-                <div className={`${hrdashboard.__posts} ${selectedJobId === data._id ? hrdashboard.activeCard : ''}`} key={data._id} onClick={() => handleJobCardClick(data._id)}>
-                  <div className={hrdashboard.__postTitle}>
-                    <img className={hrdashboard.__postLogo} src={data.jobPoster} alt='' />
-                    <p>
-                      {data.jobTitle.slice(0, 15)}...
-                      <span style={{ fontSize: "13px", display: 'block' }}>
-                        <CalculateTimeAgo time={data.createdAt} />
-                      </span>
-                    </p>
-                    <FontAwesomeIcon className={hrdashboard.__btn_PostOpen} icon={faArrowUpRightFromSquare} />
+              itemClass="carousel-item-padding-40-px"
+            >
+              {jobPost.length > 0 &&
+                jobPost.map((data) => (
+                  <div
+                    className={`${hrdashboard.__posts} ${selectedJobId === data._id ? hrdashboard.activeCard : ""
+                      }`}
+                    key={data._id}
+                    onClick={() => handleJobCardClick(data._id)}
+                  >
+                    <div className={hrdashboard.__postTitle}>
+                      <img
+                        className={hrdashboard.__postLogo}
+                        src={data.jobPoster}
+                        alt=""
+                      />
+                      <p>
+                        {data.jobTitle.slice(0, 15)}...
+                        <span style={{ fontSize: "13px", display: "block" }}>
+                          <CalculateTimeAgo time={data.createdAt} />
+                        </span>
+                      </p>
+                      <FontAwesomeIcon
+                        className={hrdashboard.__btn_PostOpen}
+                        icon={faArrowUpRightFromSquare}
+                      />
+                    </div>
+                    <div className={hrdashboard.__post_body}>
+                      <span>{data.location}</span>
+                      <span>{data.jobExperience} years</span>
+                    </div>
+                    <div className={hrdashboard.__post_Footer}>
+                      {" "}
+                      <span>
+                        {data.totalApplication ? data.totalApplication : 0}
+                      </span>{" "}
+                      application(s){" "}
+                    </div>
                   </div>
-                  <div className={hrdashboard.__post_body}>
-                    <span>{data.location}</span>
-                    <span>{data.jobExperience} years</span>
-                  </div>
-                  <div className={hrdashboard.__post_Footer}> <span>{data.totalApplication ? data.totalApplication : 0}</span> application(s)  </div>
-                </div>
-              ))}
+                ))}
             </Carousel>
           </div>
 
           {selectedJobId && (
             <div className={hrdashboard.__latest_Post}>
               <section className={hrdashboard.__latestPosts}>
-                <HrJobDetail jobId={selectedJobId} ShowApplicantDetails={ShowApplicantDetails}
-                  CbToggleDetails={setShowApplicantDetails} />
+                <HrJobDetail
+                  jobId={selectedJobId}
+                  ShowApplicantDetails={ShowApplicantDetails}
+                  CbToggleDetails={setShowApplicantDetails}
+                />
               </section>
             </div>
           )}
@@ -162,29 +228,62 @@ export default function HRDashboard() {
             <div className={hrdashboard.__latest_Post}>
               <div className={hrdashboard.__latest_Post_Header}>
                 <h3>Latest Post</h3>
-                <p className=''>see all</p>
+                <p className="">see all</p>
               </div>
               <section className={hrdashboard.__latestPosts}>
-                {sortedJob.map((jobs) => (
-                  <div className={`${hrdashboard.__user_Post} ${selectedJobId === jobs._id ? hrdashboard.activeCard : ''}`} key={jobs._id} onClick={() => handleJobCardClick(jobs._id)}>
-                    <div className={hrdashboard.__user_Post_Header}>
-                      <img className={hrdashboard.__user_PostImg} src={user} alt="" />
-                      <div>
-                        <h3 style={{ fontSize: '20px' }}>{localStorage.name}</h3>
-                        <span className={hrdashboard.__user_Position}>HR Executive</span>
+                {
+                  sortedJob.length === 0 ? <h2>No Job Found</h2> : <>
+
+                    {sortedJob.map((jobs, index) => (
+                      <div
+                        className={`${hrdashboard.__user_Post} ${selectedJobId === jobs._id ? hrdashboard.activeCard : ""
+                          }`}
+                        key={index}
+                        onClick={() => handleJobCardClick(jobs._id)}
+                      >
+                        <div className={hrdashboard.__user_Post_Header}>
+                          <img
+                            className={hrdashboard.__user_PostImg}
+                            src={user}
+                            alt=""
+                          />
+                          <div>
+                            <h3 style={{ fontSize: "20px" }}>
+                              {localStorage.name}
+                            </h3>
+                            <span className={hrdashboard.__user_Position}>
+                              HR Executive
+                            </span>
+                          </div>
+                          <FontAwesomeIcon
+                            icon={faTrash}
+                            title="delete post"
+                            className={hrdashboard.createPost_BtnDelete}
+                            onClick={() => handleDelete(jobs._id)}
+                          />
+                        </div>
+                        <div className={hrdashboard.__user_Post_body}>
+                          <img
+                            className={hrdashboard.__latestPosts_Img}
+                            src={jobs.jobPoster}
+                            alt=""
+                          />
+                          <p className={hrdashboard.__user_Post_info}>
+                            {jobs.jobDescription}
+                          </p>
+                        </div>
+                        <div className={hrdashboard.__user_Post_Footer}>
+                          <h6 className={hrdashboard.__user_Post_Timestamp}>
+                            {formattedDate(jobs.createdAt)}
+                          </h6>
+                          <button className={hrdashboard.__btn_Repost}>
+                            Repost
+                          </button>
+                        </div>
                       </div>
-                      <FontAwesomeIcon icon={faTrash} title="delete post" className={hrdashboard.createPost_BtnDelete} onClick={() => handleDelete(jobs._id)} />
-                    </div>
-                    <div className={hrdashboard.__user_Post_body}>
-                      <img className={hrdashboard.__latestPosts_Img} src={jobs.jobPoster} alt="" />
-                      <p className={hrdashboard.__user_Post_info}>{jobs.jobDescription}</p>
-                    </div>
-                    <div className={hrdashboard.__user_Post_Footer}>
-                      <h6 className={hrdashboard.__user_Post_Timestamp}>{formattedDate(jobs.createdAt)}</h6>
-                      <button className={hrdashboard.__btn_Repost}>Repost</button>
-                    </div>
-                  </div>
-                ))}
+                    ))}
+                  </>
+                }
               </section>
             </div>
           )}
--- frontend/src/pages/Employer/components/TopBar.js
+++ frontend/src/pages/Employer/components/TopBar.js
@@ -1,71 +1,94 @@
-import React, { useEffect, useState } from 'react'
-// import axios from 'axios'
-import layout from './RecruiterLayout.module.css';
+import React, { useEffect, useState } from "react";
+import layout from "./RecruiterLayout.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass, faMicrophone, faUserPlus } from "@fortawesome/free-solid-svg-icons";
-// import Loader from '../../Common-Components/Loaders/Loader';
+import {
+  faMagnifyingGlass,
+  faUserPlus,
+} from "@fortawesome/free-solid-svg-icons";
+import { IoMicOutline } from "react-icons/io5";
+import { IoMicOffOutline } from "react-icons/io5";
+import { handleSearchData } from "../../../Redux/ReduxFilterSlice";
 import { VscSettings } from "react-icons/vsc";
 import { IoIosNotificationsOutline } from "react-icons/io";
-import { useNavigate } from 'react-router-dom';
-import { io } from "socket.io-client"
-import Badge from '@mui/material/Badge';
+import { useNavigate } from "react-router-dom";
+import { io } from "socket.io-client";
+import Badge from "@mui/material/Badge";
 import { useSelector } from "react-redux";
 import NotificationBox from "../../Common-Components/NotificationBox";
 import axios from "axios";
-const baseUrl = process.env.REACT_APP_BACKEND_BASE_URL
+import { useDispatch } from "react-redux";
+const baseUrl = process.env.REACT_APP_BACKEND_BASE_URL;
 export default function TopBar() {
   const socket = io("http://localhost:8080");
   const { email } = useSelector((state) => state.Assessment.currentUser);
+  const [searhOption, setSearchOption] = useState({ searchText: "" });
   const [notificationCount, setNotificationCount] = useState(0);
   const [ToggleNotification, SetToggleNotification] = useState(false);
-  // const [loading, setLoading] = useState(false)
-  // const [jobs, setJobs] = useState([]);
-  // const [filteredJobs, setFilteredJobs] = useState([]);
-  // const [searchTerm, setSearchTerm] = useState('');
-  const navigateTo = useNavigate()
+  const [isListening, setIsListening] = useState(false);
+  const navigateTo = useNavigate();
+  const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   const fetchJobs = async () => {
-  //     try {
-  //       setLoading(true)
-  //       const response = await axios.get('http://localhost:8080/api/jobs/All-jobs');
-  //       setJobs(response.data.jobs);
-  //       setFilteredJobs(response.data.jobs);
-  //       setLoading(false)
-  //     } catch (error) {
-  //       console.error("Error: ", error);
-  //     }
-  //   };
-  //   fetchJobs();
-  // }, []);
+  const recognition = new window.webkitSpeechRecognition(); // Initialize speech recognition
 
-  // Filter jobs based on search term
-  // const handleSearch = (e) => {
-  //   const searchTerm = e.target.value.toLowerCase();
-  //   setSearchTerm(searchTerm);
+  recognition.continuous = false; // Enable continuous listening
+  recognition.lang = "en-US"; // Set the language for speech recognition
 
-  //   const filteredJobs = jobs.filter((job) => {
-  //     return (
-  //       job.title.toLowerCase().includes(searchTerm) ||
-  //       job.description.toLowerCase().includes(searchTerm)
-  //     );
-  //   });
-  //   setFilteredJobs(filteredJobs);
-  // };
+  recognition.onresult = (event) => {
+    const transcript = event.results[event.results.length - 1][0].transcript;
+    setSearchOption({ ...searhOption, "searchText": transcript });
+    setIsListening(false);
+  };
+
+  recognition.onerror = (event) => {
+    console.error("Speech recognition error:", event.error);
+  };
+
+  const toggleMicListening = (e) => {
+    e.preventDefault();
+    if (isListening) {
+      recognition.stop(); // Stop speech recognition if it's currently listening
+      setIsListening(false);
+    } else {
+      recognition.start(); // Start speech recognition
+      setIsListening(true);
+    }
+  };
+  const handleSearchInputChange = (e) => {
+    setSearchOption({ ...searhOption, [e.target.name]: e.target.value });
+  };
+
+  useEffect(() => {
+    const setTextTimeOut = setTimeout(() => {
+      if (searhOption.searchText || searhOption.Location) {
+        dispatch(handleSearchData(searhOption));
+      } else {
+        dispatch(handleSearchData(searhOption));
+      }
+    }, 1000);
+    return () => clearTimeout(setTextTimeOut);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searhOption]);
 
   // !Load Notifications
-
   const LoadNotifications = () => {
-    axios.get(`${baseUrl}/user/notifications/get-notification/${email}`).then((response) => {
-      if (response.data.success) {
-        setNotificationCount(response.data.notification.filter((data) => data.notificationStatus.toLowerCase() === 'Unread'.toLowerCase()).length);
-      } else {
-        setNotificationCount(0);
-      }
-    }).catch((error) => {
-      console.log(error)
-    })
-  }
+    axios
+      .get(`${baseUrl}/user/notifications/get-notification/${email}`)
+      .then((response) => {
+        if (response.data.success) {
+          setNotificationCount(
+            response.data.notification.filter(
+              (data) =>
+                data.notificationStatus.toLowerCase() === "Unread".toLowerCase()
+            ).length
+          );
+        } else {
+          setNotificationCount(0);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   useEffect(() => {
     socket.emit("userConnect", JSON.stringify({ userEmail: email }));
@@ -75,13 +98,19 @@ export default function TopBar() {
 
   useEffect(() => {
     socket.on("receiveNotification", (data) => {
-      axios.post(`${baseUrl}/user/notifications/save-notification`, JSON.parse(data)).then((response) => {
-        if (response.data.success) {
-          LoadNotifications()
-        }
-      }).catch((error) => {
-        console.log(error)
-      })
+      axios
+        .post(
+          `${baseUrl}/user/notifications/save-notification`,
+          JSON.parse(data)
+        )
+        .then((response) => {
+          if (response.data.success) {
+            LoadNotifications();
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     });
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -90,22 +119,55 @@ export default function TopBar() {
     <>
       <div className={layout.__topbar}>
         <div className={layout.__searchbar}>
-          <FontAwesomeIcon className={layout.__topbar_Icon} icon={faMagnifyingGlass} />
-          <input className={layout.__input} type="text" placeholder='search by keyword......'
-          // value={searchTerm} 
-          // onChange={handleSearch} 
+          <FontAwesomeIcon
+            className={layout.__topbar_Icon}
+            icon={faMagnifyingGlass}
           />
-          <FontAwesomeIcon className={layout.__topbar_Icon} icon={faMicrophone} />
+          <input
+            className={layout.__input}
+            type="text"
+            name="searchText"
+            id="searchText"
+            placeholder="search by keyword......"
+            onChange={handleSearchInputChange}
+            value={searhOption.searchText}
+          />
+          {isListening ? (
+            <IoMicOutline
+              className={layout.__topbar_Icon}
+              onClick={toggleMicListening}
+            />
+          ) : (
+            <IoMicOffOutline
+              className={layout.__topbar_Icon}
+              onClick={toggleMicListening}
+            />
+          )}
         </div>
-        <button onClick={() => navigateTo('/addemployee')} className={layout.__btn_Add_Employee}> <FontAwesomeIcon icon={faUserPlus} /> Add Employee</button>
-        <VscSettings className={layout.__btn_filter} style={{ color: 'white', fontSize: '25' }} />
+        <button
+          onClick={() => navigateTo("/addemployee")}
+          className={layout.__btn_Add_Employee}
+        >
+          <FontAwesomeIcon icon={faUserPlus} /> Add Employee
+        </button>
+        <VscSettings
+          className={layout.__btn_filter}
+          style={{ color: "white", fontSize: "25" }}
+        />
         <Badge color="primary" badgeContent={notificationCount}>
-          <IoIosNotificationsOutline className={layout.__btn_notfication} style={{ color: 'white', fontSize: '25' }} onClick={() => SetToggleNotification(!ToggleNotification)} />
+          <IoIosNotificationsOutline
+            className={layout.__btn_notfication}
+            style={{ color: "white", fontSize: "25" }}
+            onClick={() => SetToggleNotification(!ToggleNotification)}
+          />
         </Badge>
       </div>
-      {
-        ToggleNotification && <NotificationBox notificationCounter={setNotificationCount} CbCloseNotification={SetToggleNotification} />
-      }
+      {ToggleNotification && (
+        <NotificationBox
+          notificationCounter={setNotificationCount}
+          CbCloseNotification={SetToggleNotification}
+        />
+      )}
     </>
-  )
+  );
 }
